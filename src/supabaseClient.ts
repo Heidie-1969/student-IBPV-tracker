6d0dcb2141d8c4d356d5f70bcc01e4e6b595b242
Heidie-1969 <heidievanremortele@gmail.com>
2026-06-11 20:07:44 +0200

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 
 // Vervang de teksten hieronder door jouw gegevens uit het Supabase-scherm
 const supabaseUrl = 'https://dniwiexpymmndxmtuoih.supabase.co';
-const supabaseAnonKey = 'sb_publishable_vaNYXYGo-yEBvsMszecZdw_HdpuFi_E';
+const supabaseAnonKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRuaXdpZXhweW1tbmR4bXR1b2loIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA1ODkzMDUsImV4cCI6MjA5NjE2NTMwNX0.Zu3KqebQjWySP60HoSTR-MQhnjs1-kc-CkIQZMavHT4
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
